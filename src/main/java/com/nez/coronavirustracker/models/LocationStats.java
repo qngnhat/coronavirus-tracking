59c3cb711e3126a68c0c6f49d236e1e68e929954
qngnhat <qngnhat@gmail.com>
2021-03-23 15:18:33 +0700

    2nd commit

---
+++ src/main/java/com/nez/coronavirustracker/models/LocationStats.java
@@ -0,0 +1,44 @@
+package com.nez.coronavirustracker.models;
+
+
+public class LocationStats {
+	private String states;
+	private String country;
+	private int latestTotalCases;
+	private int diffFromPrevDay;
+	public String getStates() {
+		return states;
+	}
+	public void setStates(String states) {
+		this.states = states;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public int getLatestTotalCases() {
+		return latestTotalCases;
+	}
+	public void setLatestTotalCases(int latestTotalCases) {
+		this.latestTotalCases = latestTotalCases;
+	}
+	
+	public int getDiffFromPrevDay() {
+		return diffFromPrevDay;
+	}
+	public void setDiffFromPrevDay(int diffFromPrevDay) {
+		this.diffFromPrevDay = diffFromPrevDay;
+	}
+	@Override
+	public String toString() {
+		return "LocationStats [states=" + states + ", country=" + country + ", latestTotalCases=" + latestTotalCases
+				+ ", diffFromPrevDay=" + diffFromPrevDay + "]";
+	}
+	
+	
+	
+	
+	
+}
